cd2b1c2417291460739f120f1f97f08491ed1c61
s_kawamoto <s_kawamoto@users.sourceforge.jp>
2014-05-24 18:34:17 +0900

    Fix installers.

--- Package/make_exe_file.bat
+++ Package/make_exe_file.bat
@@ -7,8 +7,8 @@ call installer_config.bat
 set EXEPRESS_PATH="%ProgramFiles%\Web Technology\EXEpress CX 5\EXEpress.exe"
 if %PROCESSOR_ARCHITECTURE%==AMD64 set EXEPRESS_PATH="%ProgramFiles(x86)%\Web Technology\EXEpress CX 5\EXEpress.exe"
 
-%EXEPRESS_PATH% %cd%\%INI_INST_JPN%
-%EXEPRESS_PATH% %cd%\%INI_INST_ENG%
-%EXEPRESS_PATH% %cd%\%AMD64_INI_INST_JPN%
-%EXEPRESS_PATH% %cd%\%AMD64_INI_INST_ENG%
+%EXEPRESS_PATH% "%cd%\%INI_INST_JPN%"
+%EXEPRESS_PATH% "%cd%\%INI_INST_ENG%"
+%EXEPRESS_PATH% "%cd%\%AMD64_INI_INST_JPN%"
+%EXEPRESS_PATH% "%cd%\%AMD64_INI_INST_ENG%"
 
