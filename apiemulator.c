f8dc74e64bfd32eb561e76bc4f9435a82a012fbc
s_kawamoto <s_kawamoto@users.sourceforge.jp>
2014-06-21 20:45:30 +0900

    Fix the commit.

---
+++ apiemulator.c
@@ -0,0 +1,28 @@
+﻿// apiemulator.c
+// Copyright (C) 2014 Suguru Kawamoto
+// APIエミュレータ
+
+#include <windows.h>
+
+#define DO_NOT_REPLACE
+#include "apiemulator.h"
+
+BOOL IsUserAnAdminAlternative()
+{
+	BOOL bResult;
+	SID_IDENTIFIER_AUTHORITY sia = SECURITY_NT_AUTHORITY;
+	PSID pSID;
+	BOOL b;
+	bResult = FALSE;
+	if(AllocateAndInitializeSid(&sia, 2, SECURITY_BUILTIN_DOMAIN_RID, DOMAIN_ALIAS_RID_ADMINS, 0, 0, 0, 0, 0, 0, &pSID))
+	{
+		if(CheckTokenMembership(NULL, pSID, &b))
+		{
+			if(b)
+				bResult = TRUE;
+		}
+		FreeSid(pSID);
+	}
+	return bResult;
+}
+
---
+++ apiemulator.h
@@ -0,0 +1,19 @@
+﻿// apiemulator.h
+// Copyright (C) 2014 Suguru Kawamoto
+// APIエミュレータ
+
+#ifndef __APIEMULATOR_H__
+#define __APIEMULATOR_H__
+
+#include <windows.h>
+
+#ifndef DO_NOT_REPLACE
+
+#define IsUserAnAdmin IsUserAnAdminAlternative
+
+#endif
+
+BOOL IsUserAnAdminAlternative();
+
+#endif
+
