64fe62e19689f633c9fc043086ba5f1dd4ca140a
s_kawamoto <s_kawamoto@users.sourceforge.jp>
2014-10-30 21:12:58 +0900

    Change the installer into EXEpress 6.

--- Package/make_exe_file.bat
+++ Package/make_exe_file.bat
@@ -4,8 +4,8 @@ REM;
 
 call installer_config.bat
 
-set EXEPRESS_PATH="%ProgramFiles%\Web Technology\EXEpress CX 5\EXEpress.exe"
-if %PROCESSOR_ARCHITECTURE%==AMD64 set EXEPRESS_PATH="%ProgramFiles(x86)%\Web Technology\EXEpress CX 5\EXEpress.exe"
+set EXEPRESS_PATH="%ProgramFiles%\Web Technology\EXEpress 6\EXEpress.exe"
+if %PROCESSOR_ARCHITECTURE%==AMD64 set EXEPRESS_PATH="%ProgramFiles(x86)%\Web Technology\EXEpress 6\EXEpress.exe"
 
 %EXEPRESS_PATH% "%cd%\%INI_INST_JPN%"
 %EXEPRESS_PATH% "%cd%\%INI_INST_ENG%"
