3ad83fb7722e6ba759724afbc2b80d18d8a52f77
s_kawamoto <s_kawamoto@users.sourceforge.jp>
2017-01-22 16:19:55 +0900

    Update PEM file. Rebuild OpenSSL with backward compatibility options.

--- OpenSSL/buildx86.bat
+++ OpenSSL/buildx86.bat
@@ -6,7 +6,7 @@ rem Use compat51.bat to support Windows XP or later.
 rem Use compat50.bat to support Windows 2000.
 call compat50.bat
 perl nodebug.pl
-nmake /f makefile
+nmake /f makefile build_libs
 copy /y libeay32.dll %var1%\libeay32.dll
 copy /y ssleay32.dll %var1%\ssleay32.dll
 popd
