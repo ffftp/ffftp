242de6499f45c527d66799ff89abf06c2305f563
Kurata Sayuri <MagicalSayuri@gmail.com>
2019-03-18 21:32:35 +0900

    update to v3.6.

--- common.h
+++ common.h
@@ -232,9 +232,9 @@ constexpr FileType AllFileTyes[]{ FileType::All, FileType::Executable, FileType:
 /*===== バージョン ======*/
 
 #ifdef _WIN64
-#define VER_STR					"3.6 64bit"
+#define VER_STR					"3.7 64bit"
 #else
-#define VER_STR					"3.6 32bit"
+#define VER_STR					"3.7 32bit"
 #endif
 #define VER_NUM					2000		/* 設定バージョン */
 
