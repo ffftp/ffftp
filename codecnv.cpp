f2db8988d2c1b471b1d907d34a33ed63a71f1791
Kurata Sayuri <MagicalSayuri@gmail.com>
2021-07-31 10:54:03 +0900

    fix #301, change escape sequence.

--- codecnv.cpp
+++ codecnv.cpp
@@ -90,7 +90,8 @@ void CodeDetector::Test(std::string_view str) {
 
 
 static void fullwidth(std::wstring& str) {
-	static boost::wregex re{ LR"([\uFF61-\uFF9F]+)" };
+	// 半角カタカナ ｡ \uFF61 ～ ﾟ \uFF9F
+	static boost::wregex re{ LR"([｡-ﾟ]+)" };
 	str = replace<wchar_t>(str, re, [](auto const& m) { return NormalizeString(NormalizationKC, { m[0].first, (size_t)m.length() }); });
 }
 
@@ -286,7 +287,7 @@ std::string ConvertTo(std::string_view str, int kanji, int kana) {
 			// U+02000–U+02FFF       2000-2FFF
 			// U+0F900–U+0FAFF       F900-FAFF
 			// U+2F800–U+2FAFF  D87E DC00-DEFF
-			static boost::wregex re{ LR"((.*?)((?:[\u2000-\u2FFF\uF900-\uFAFF]|\uD87E[\uDC00-\uDEFF])+|$))" };
+			static boost::wregex re{ LR"((.*?)((?:[\x{2000}-\x{2FFF}\x{F900}-\x{FAFF}]|\x{D87E}[\x{DC00}-\x{DEFF}])+|$))" };
 			str = replace<wchar_t>(str, re, [](auto const& m) { return NormalizeString(NormalizationD, { m[1].first, (size_t)m.length(1) }).append(m[2].first, m[2].second); });
 		});
 	case KANJI_UTF8N:
